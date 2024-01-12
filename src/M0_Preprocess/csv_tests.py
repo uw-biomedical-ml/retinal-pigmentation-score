833d2ff02e1f989cf5d42e9adbcfec238494bc15
arajesh17 <anandwa@gmail.com>
2024-01-12 10:09:16 -0800

    added csv_tests

---
+++ src/M0_Preprocess/csv_tests.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import os
+
+
+class CSVValidationError(Exception):
+    """Custom exception for CSV validation errors."""
+    pass
+
+
+def validate_csv(filename):
+    # Check if the filename ends with '.csv'
+    if not filename.endswith('.csv'):
+        raise CSVValidationError("Inputted csv at {0} does not end with .csv".format(filename))
+
+    # Check if the file exists
+    if not os.path.isfile(filename):
+        raise CSVValidationError("Inputed csv at {0} does not exist".format(filename))
+
+    # Read and check for 'images' column
+    try:
+        df = pd.read_csv(filename)
+        if 'path' not in df.columns:
+            raise CSVValidationError("{0} does not contain 'path' column".format(filename))
+        if not os.path.isfile(df['path'][0]):
+            raise CSVValidationError("first element {0} does not exist, check that filenames have the correct path in your input csv".format(
+                df['path'][0]
+            ))
+    except Exception as e:
+        raise CSVValidationError(f"Error reading CSV file: {e}")
+
+    print("{} is valid".format(filename))
+    
